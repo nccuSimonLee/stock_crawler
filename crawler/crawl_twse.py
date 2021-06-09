a93f394ca6bf2362230ef63e702cb22a68d62f8a
nccuSimonLee <simon2020291@gmail.com>
2021-06-09 23:48:51 +0000

    [refactor] move the crawler modules to crawler/

--- crawler/crawl_twse.py
+++ crawler/crawl_twse.py
@@ -1,14 +1,16 @@
-from stock_price_api import request_tpex_prices
+from stock_price_api import request_twse_prices
 import time
 
 
 
+RAW_FIELDS = ['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']
 
 def crawl_and_save_months_prices(stock_id, year, months, f):
     for month in months:
         print(f'{stock_id} - {year} - {month}')
-        results = request_tpex_prices(stock_id, year, month).json()
-        for price in preprocess_prices(results['aaData']):
+        results = request_twse_prices(stock_id, year, month).json()
+        assert results['fields'] == RAW_FIELDS
+        for price in preprocess_prices(results['data']):
             f.write(f'{",".join(price)}\n')
         time.sleep(10)
     return
@@ -19,17 +21,17 @@ def preprocess_prices(prices):
         year, month, day = price[0].split('/')
         year = str(int(year) + 1911)
         price = [p.replace(',', '') for p in price[1:]]
-        for i in range(2):
-            # 因為前兩項是仟股、仟元
-            price[i] = str(int(price[i]) * 1000)
         price[-2] = price[-2].replace('+', '')
         price = [year, month, day] + price
         preprocessed_prices.append(price)
     return preprocessed_prices
+        
 
 def main():
-
-    stock_list = ['3105', '4743', '6547']
+    with open('new_top_30_stock.txt', 'r') as f:
+        stock_list = f.read().splitlines()
+    
+    stock_list = ['0050', '2330', '2376', '2409', '2417']
 
     for stock in stock_list:
         with open(f'prices/{stock}.csv', 'w') as f:
@@ -37,6 +39,7 @@ def main():
         f = open(f'prices/{stock}.csv', 'a')
         crawl_and_save_months_prices(stock, 2021, range(5, 0, -1), f)
         crawl_and_save_months_prices(stock, 2020, range(12, 0, -1), f)
+        f.close()
 
 if __name__ == '__main__':
     main()
