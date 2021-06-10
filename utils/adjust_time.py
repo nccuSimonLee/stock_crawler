9df5b4ce5ee6bc7fa0736f4a2bc5394f11e9e88f
nccuSimonLee <simon2020291@gmail.com>
2021-06-10 12:43:22 +0000

    [add] add `cmoney_time_to_dt()` to adjust_time.py

--- utils/adjust_time.py
+++ utils/adjust_time.py
@@ -7,6 +7,9 @@ def ptt_time_to_dt(time_str):
     dt = datetime.strptime(time_str, '%a %b %d %H:%M:%S %Y')
     return dt
 
+def cmoney_time_to_dt(time_str):
+    return datetime.strptime(time_str, '%Y/%m/%d %H:%M')
+
 def adjust_dt(raw_dt):
     backoff_start, backoff_end = get_backoff_span(raw_dt)
     if backoff_start <= raw_dt < backoff_end:
