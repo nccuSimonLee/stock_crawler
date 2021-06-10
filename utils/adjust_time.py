9533b8e4181625f6fad8cd0a74a6ddd8df27b71d
nccuSimonLee <simon2020291@gmail.com>
2021-06-10 01:04:49 +0000

    [add] create `adjust_time.py` and add functions to it

---
+++ utils/adjust_time.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+
+
+def ptt_time_to_dt(time_str):
+    time_str = time_str.split('/')[0]
+    dt = datetime.strptime(time_str, '%a %b %d %H:%M:%S %Y')
+    return dt
+
+def adjust_dt(raw_dt):
+    backoff_start, backoff_end = get_backoff_span(raw_dt)
+    if backoff_start <= raw_dt < backoff_end:
+        dt = raw_dt - timedelta(days=1)
+    else:
+        dt = raw_dt
+    dt = datetime(dt.year, dt.month, dt.day)
+    return dt
+
+def get_backoff_span(dt):
+    backoff_start = datetime(
+        dt.year,
+        dt.month,
+        dt.day,
+        0,
+        0,
+        0
+    )
+    backoff_end = datetime(
+        dt.year,
+        dt.month,
+        dt.day,
+        13,
+        30,
+        0
+    )
+    return (backoff_start, backoff_end)
